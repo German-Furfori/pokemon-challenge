c830b5cdfef73272dbb87a247e92c4dd4485bd7f
Germán Furfori <german.furfori@paygoal.com.ar>
2025-09-06 22:36:23 +0200

    First integration test for /pokemon/{name}

--- src/test/java/com/truelayer/pokeapp/PokeappApplicationTests.java
+++ src/test/java/com/truelayer/pokeapp/PokeappApplicationTests.java
@@ -1,13 +1,34 @@
 package com.truelayer.pokeapp;
 
-import org.junit.jupiter.api.Test;
+import com.truelayer.pokeapp.dto.poke.FlavorDto;
+import com.truelayer.pokeapp.dto.poke.HabitatDto;
+import com.truelayer.pokeapp.dto.poke.PokeApiResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.List;
 
 @SpringBootTest
-class PokeappApplicationTests {
+@AutoConfigureMockMvc
+@AutoConfigureMockRestServiceServer
+public abstract class PokeappApplicationTests {
+	@Autowired
+	protected MockMvc mockMvc;
 
-	@Test
-	void contextLoads() {
+	protected PokeApiResponseDto getPokeApiResponse() {
+		FlavorDto flavor = new FlavorDto();
+		flavor.setFlavorText("Pokemon description");
+
+		HabitatDto habitat = new HabitatDto();
+		habitat.setName("Habitat name");
+
+		PokeApiResponseDto pokeApiResponseDto = new PokeApiResponseDto();
+		pokeApiResponseDto.setHabitat(habitat);
+		pokeApiResponseDto.setFlavorTextEntries(List.of(flavor));
+		pokeApiResponseDto.setIsLegendary(false);
+
+		return pokeApiResponseDto;
 	}
-
 }
---
+++ src/test/java/com/truelayer/pokeapp/controller/PokeControllerTest.java
@@ -0,0 +1,48 @@
+package com.truelayer.pokeapp.controller;
+
+import com.truelayer.pokeapp.PokeappApplicationTests;
+import com.truelayer.pokeapp.webclient.PokeApiWebClient;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class PokeControllerTest extends PokeappApplicationTests {
+    @MockitoBean
+    private PokeApiWebClient pokeApiWebClient;
+    private final String pathPokemon = "/pokemon/{name}";
+    private static final String DEFAULT_POKEMON = "pikachu";
+
+    @Test
+    @SneakyThrows
+    void findPokemonInfo_withCorrectFields_returnPokemonInfo() {
+        ArgumentCaptor<String> argumentCaptorName = ArgumentCaptor.forClass(String.class);
+
+        given(pokeApiWebClient.getPokemonInfo(argumentCaptorName.capture()))
+                .willReturn(this.getPokeApiResponse());
+
+        mockMvc.perform(get(pathPokemon, DEFAULT_POKEMON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.description").exists())
+                .andExpect(jsonPath("$.habitat").exists())
+                .andExpect(jsonPath("$.isLegendary").exists())
+                .andExpect(jsonPath("$.name").value("pikachu"))
+                .andExpect(jsonPath("$.description").value("Pokemon description"))
+                .andExpect(jsonPath("$.habitat").value("Habitat name"))
+                .andExpect(jsonPath("$.isLegendary").value(false));
+
+        assertEquals("pikachu", argumentCaptorName.getValue());
+
+        verify(pokeApiWebClient, times(1)).getPokemonInfo(argumentCaptorName.getValue());
+    }
+}
