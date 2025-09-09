85dec9b48676abc26a1decf0ef8bc8445194ca4c
Germán Furfori <german.furfori@paygoal.com.ar>
2025-09-09 20:03:54 +0200

    Unit test for verifying translations for translatedPokemonInfo endpoint

--- src/test/java/com/truelayer/pokeapp/controller/PokeControllerTest.java
+++ src/test/java/com/truelayer/pokeapp/controller/PokeControllerTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
 import java.util.Optional;
 import java.util.stream.Stream;
 
--- src/test/java/com/truelayer/pokeapp/service/PokeServiceTest.java
+++ src/test/java/com/truelayer/pokeapp/service/PokeServiceTest.java
@@ -2,6 +2,7 @@ package com.truelayer.pokeapp.service;
 
 import com.truelayer.pokeapp.dto.api.PokeInfoResponseDto;
 import com.truelayer.pokeapp.dto.poke.PokeApiResponseDto;
+import com.truelayer.pokeapp.dto.translation.TranslateResponseDto;
 import com.truelayer.pokeapp.exception.PokeApiGenericException;
 import com.truelayer.pokeapp.exception.PokeApiNotFoundException;
 import com.truelayer.pokeapp.mapper.PokeMapper;
@@ -9,9 +10,14 @@ import com.truelayer.pokeapp.service.impl.PokeServiceImpl;
 import com.truelayer.pokeapp.webclient.PokeApiWebClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.truelayer.pokeapp.constant.DefaultValues.DEFAULT_DESCRIPTION;
 import static com.truelayer.pokeapp.constant.DefaultValues.DEFAULT_HABITAT;
@@ -19,6 +25,7 @@ import static com.truelayer.pokeapp.constant.DefaultValues.DEFAULT_IS_LEGENDARY;
 import static com.truelayer.pokeapp.constant.DefaultValues.DEFAULT_POKEMON;
 import static com.truelayer.pokeapp.util.TestUtils.getPokeApiResponse;
 import static com.truelayer.pokeapp.util.TestUtils.getPokeInfoResponse;
+import static com.truelayer.pokeapp.util.TestUtils.getTranslationResponse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -34,8 +41,19 @@ public class PokeServiceTest {
     private PokeApiWebClient pokeApiWebClient;
     @Mock
     private PokeMapper pokeMapper;
+    @Mock
+    private TranslationService translationService;
     @InjectMocks
     private PokeServiceImpl pokeService;
+
+    private static Stream<Arguments> provideDataForTranslationTypes() {
+        return Stream.of(
+                Arguments.of("shakespeare", "habitat", false, getTranslationResponse().get(), 1),
+                Arguments.of("yoda", "cave", false, getTranslationResponse().get(), 1),
+                Arguments.of("yoda", "habitat", true, getTranslationResponse().get(), 1),
+                Arguments.of("yoda", "habitat", true, null, 0)
+        );
+    }
 
     @Test
     void findPokemonInfo_shouldReturnMappedResponse() {
@@ -79,5 +97,29 @@ public class PokeServiceTest {
 
         verify(pokeApiWebClient, times(1)).getPokemonInfo(DEFAULT_POKEMON);
         verify(pokeMapper, never()).fromPokeApiResponseToPokeInfoResponse(any(), any());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDataForTranslationTypes")
+    void findPokemonTranslatedInfo_shouldUpdateDescriptionWhenTranslationIsPresent(String translationType,
+                                                                                   String habitat,
+                                                                                   Boolean isLegendary,
+                                                                                   TranslateResponseDto translate,
+                                                                                   Integer mapperTimesExpected) {
+        PokeApiResponseDto mockApiResponse = getPokeApiResponse(DEFAULT_DESCRIPTION, habitat, isLegendary);
+        PokeInfoResponseDto pokeInfo = getPokeInfoResponse(DEFAULT_POKEMON, DEFAULT_DESCRIPTION, habitat, isLegendary);
+        Optional<TranslateResponseDto> translateResponse = Optional.ofNullable(translate);
+
+        when(pokeApiWebClient.getPokemonInfo(DEFAULT_POKEMON))
+                .thenReturn(mockApiResponse);
+        when(pokeMapper.fromPokeApiResponseToPokeInfoResponse(DEFAULT_POKEMON, mockApiResponse))
+                .thenReturn(pokeInfo);
+        when(translationService.translate(DEFAULT_DESCRIPTION, translationType))
+                .thenReturn(translateResponse);
+
+        pokeService.findPokemonTranslatedInfo(DEFAULT_POKEMON);
+
+        verify(translationService, times(1)).translate(DEFAULT_DESCRIPTION, translationType);
+        verify(pokeMapper, times(mapperTimesExpected)).updatePokemonDescription(translate, pokeInfo);
     }
 }
